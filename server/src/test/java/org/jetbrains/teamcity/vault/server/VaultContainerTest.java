829074d1c8ef0744df79f8dbdb07e85ebb5010cd
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-08-06 21:12:27 +0300

    Add simple test using testcontainers

---
+++ server/src/test/java/org/jetbrains/teamcity/vault/server/VaultContainerTest.java
@@ -0,0 +1,32 @@
+package org.jetbrains.teamcity.vault.server;
+
+import org.jetbrains.teamcity.vault.VaultDevContainer;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.vault.config.ClientHttpRequestFactoryFactory;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.ClientOptions;
+import org.springframework.vault.support.SslConfiguration;
+import org.springframework.vault.support.VaultHealth;
+import org.springframework.vault.support.VaultToken;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class VaultContainerTest {
+    @ClassRule
+    public static final VaultDevContainer vault = new VaultDevContainer();
+
+    @Test
+    public void testVaultIsUpAndRunning() throws Exception {
+        final ClientHttpRequestFactory factory = ClientHttpRequestFactoryFactory.create(new ClientOptions(), SslConfiguration.NONE);
+
+        final VaultTemplate template = new VaultTemplate(vault.getEndpoint(), factory, () -> VaultToken.of(vault.getToken()));
+
+        final VaultHealth health = template.opsForSys().health();
+        then(health.isInitialized()).isTrue();
+        then(health.isSealed()).isFalse();
+        then(health.isStandby()).isFalse();
+        then(health.getVersion()).isEqualTo("0.7.3");
+    }
+}
---
+++ server/src/test/kotlin/org/jetbrains/teamcity/vault/VaultDevContainer.kt
@@ -0,0 +1,24 @@
+package org.jetbrains.teamcity.vault
+
+import org.springframework.vault.client.VaultEndpoint
+import org.testcontainers.containers.GenericContainer
+import java.net.URI
+import java.util.*
+
+val vault_version = "0.7.3"
+
+class VaultDevContainer(val token: String = UUID.randomUUID().toString()) : GenericContainer<VaultDevContainer>("vault:$vault_version") {
+    init {
+        withExposedPorts(8200)
+        withEnv("VAULT_DEV_ROOT_TOKEN_ID", token)
+    }
+
+    val url: String
+        get() = "http://$containerIpAddress:$firstMappedPort"
+
+    val endpoint: VaultEndpoint
+        get() = VaultEndpoint.from(URI.create(url))
+
+
+
+}
