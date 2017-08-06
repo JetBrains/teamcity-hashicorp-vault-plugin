4fbd48ba6f67234f8d2e715354423060a45cea44
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-08-06 23:13:37 +0300

    Move common tests infrastructure to module 'common'

--- common/src/test/kotlin/org/jetbrains/teamcity/vault/VaultDevContainer.kt
+++ common/src/test/kotlin/org/jetbrains/teamcity/vault/VaultDevContainer.kt
@@ -1,13 +1,15 @@
 package org.jetbrains.teamcity.vault
 
+import org.springframework.vault.authentication.SimpleSessionManager
 import org.springframework.vault.client.VaultEndpoint
+import org.springframework.vault.support.VaultToken
 import org.testcontainers.containers.GenericContainer
 import java.net.URI
 import java.util.*
 
 val vault_version = "0.7.3"
 
-class VaultDevContainer(val token: String = UUID.randomUUID().toString()) : GenericContainer<VaultDevContainer>("vault:$vault_version") {
+class VaultDevContainer(val token: String = UUID.randomUUID().toString()) : GenericContainer<VaultDevContainer>("vault:${vault_version}") {
     init {
         withExposedPorts(8200)
         withEnv("VAULT_DEV_ROOT_TOKEN_ID", token)
@@ -19,6 +21,5 @@ class VaultDevContainer(val token: String = UUID.randomUUID().toString()) : Gene
     val endpoint: VaultEndpoint
         get() = VaultEndpoint.from(URI.create(url))
 
-
-
+    val simpleSessionManager: SimpleSessionManager get() = SimpleSessionManager { VaultToken.of(token) }
 }
