8a6580f35b7b35ce85586c710df52adcccb5ab30
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-08-08 19:38:00 +0300

    Code reuse in VaultTokenProvider

--- agent/src/main/kotlin/org/jetbrains/teamcity/vault/agent/VaultTokenProvider.kt
+++ agent/src/main/kotlin/org/jetbrains/teamcity/vault/agent/VaultTokenProvider.kt
@@ -5,12 +5,7 @@ import org.jetbrains.teamcity.vault.createRestTemplate
 import org.springframework.vault.authentication.CubbyholeAuthentication
 import org.springframework.vault.authentication.CubbyholeAuthenticationOptions
 import org.springframework.vault.authentication.LoginToken
-import org.springframework.vault.client.VaultEndpoint
-import org.springframework.vault.config.ClientHttpRequestFactoryFactory
-import org.springframework.vault.support.ClientOptions
-import org.springframework.vault.support.SslConfiguration
 import org.springframework.vault.support.VaultToken
-import java.net.URI
 
 object VaultTokenProvider {
 
@@ -20,9 +15,7 @@ object VaultTokenProvider {
                 .initialToken(VaultToken.of(wrapped))
                 .build()
 
-        val endpoint = VaultEndpoint.from(URI.create(settings.url))
-        val factory = ClientHttpRequestFactoryFactory.create(ClientOptions(), SslConfiguration.NONE)
-        val template = createRestTemplate(endpoint, factory)
+        val template = createRestTemplate(settings)
         val authentication = CubbyholeAuthentication(options, template)
 
         val token = authentication.login()
