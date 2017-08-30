7f9edfeb616448253f01fa213f6d305012ea9828
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-08-30 19:49:43 +0300

    Set env parameters by default

--- common/src/main/kotlin/org/jetbrains/teamcity/vault/util.kt
+++ common/src/main/kotlin/org/jetbrains/teamcity/vault/util.kt
@@ -62,7 +62,7 @@ fun createRestTemplate(): RestTemplate {
     return template
 }
 
-fun isShouldSetEnvParameters(parameters: MutableMap<String, String>) = parameters[VaultConstants.BehaviourParameters.ExposeEnvParameters]?.toBoolean() ?: false
+fun isShouldSetEnvParameters(parameters: MutableMap<String, String>) = parameters[VaultConstants.BehaviourParameters.ExposeEnvParameters]?.toBoolean() ?: true
 
 fun isShouldSetConfigParameters(parameters: MutableMap<String, String>) = parameters[VaultConstants.BehaviourParameters.ExposeConfigParameters]?.toBoolean() ?: false
 
