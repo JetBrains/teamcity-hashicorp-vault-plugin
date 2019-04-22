7eaf33b020c6c0b81b8180057974abcca54e49f1
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-04-22 15:38:37 +0300

    Add test for token renewal
    (cherry picked from commit 48dbb459dd9779035ff4f6c499572d68709f0ae1)

--- server/src/test/java/org/jetbrains/teamcity/vault/server/VaultConnectorTest.java
+++ server/src/test/java/org/jetbrains/teamcity/vault/server/VaultConnectorTest.java
@@ -15,26 +15,37 @@
  */
 package org.jetbrains.teamcity.vault.server;
 
+import jetbrains.buildServer.agent.BuildProgressLogger;
+import jetbrains.buildServer.agent.NullBuildProgressLogger;
 import jetbrains.buildServer.util.CollectionsUtil;
+import jetbrains.buildServer.util.ThreadUtil;
 import kotlin.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.teamcity.vault.UtilKt;
 import org.jetbrains.teamcity.vault.VaultDevContainer;
 import org.jetbrains.teamcity.vault.VaultDevEnvironment;
 import org.jetbrains.teamcity.vault.VaultFeatureSettings;
+import org.jetbrains.teamcity.vault.support.LifecycleAwareSessionManager;
 import org.jetbrains.teamcity.vault.support.VaultTemplate;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.vault.authentication.ClientAuthentication;
 import org.springframework.vault.authentication.CubbyholeAuthentication;
 import org.springframework.vault.authentication.CubbyholeAuthenticationOptions;
-import org.springframework.vault.authentication.LifecycleAwareSessionManager;
 import org.springframework.vault.support.VaultHealth;
 import org.springframework.vault.support.VaultMount;
 import org.springframework.vault.support.VaultToken;
+import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.jetbrains.teamcity.vault.UtilKt.createClientHttpRequestFactory;
@@ -102,13 +113,66 @@ public class VaultConnectorTest {
         final CubbyholeAuthentication authentication = new CubbyholeAuthentication(options, simpleTemplate);
         final TaskScheduler scheduler = new ConcurrentTaskScheduler();
 
-        final MyLifecycleAwareSessionManager sessionManager = new MyLifecycleAwareSessionManager(authentication, simpleTemplate, scheduler);
+        final MyLifecycleAwareSessionManager sessionManager = new MyLifecycleAwareSessionManager(authentication, scheduler, simpleTemplate, new LifecycleAwareSessionManager.FixedTimeoutRefreshTrigger(1L, TimeUnit.SECONDS), new NullBuildProgressLogger());
 
         then(sessionManager.getSessionToken()).isNotNull();
 
         sessionManager.renewToken();
 
         then(sessionManager.getSessionToken()).isNotNull();
+    }
+
+    @Test
+    public void testWrappedTokenAutoUpdates() {
+        doTestWrapperTokenAutoUpdates("approle-renew");
+    }
+
+    private void doTestWrapperTokenAutoUpdates(String authMountPath) {
+        final ClientHttpRequestFactory factory = createClientHttpRequestFactory();
+        final VaultTemplate template = new VaultTemplate(getVault().getEndpoint(), factory, () -> VaultToken.of(getVault().getToken()));
+
+        // Ensure approle auth enabled
+        template.opsForSys().authMount(authMountPath, VaultMount.create("approle"));
+        //myAuthMounted.add(authMountPath);
+        then(template.opsForSys().getAuthMounts()).containsKey(authMountPath + "/");
+        Map<String, Object> config = new HashMap<>();
+        config.put("secret_id_ttl", "10m");
+        config.put("token_num_uses", "10");
+        config.put("token_ttl", "5s"); // should be at least check_interval + 2 seconds
+        config.put("token_max_ttl", "30m");
+        config.put("secret_id_num_uses", "40");
+        config.put("policies", new String[]{"default"});
+        template.write("auth/" + authMountPath + "/role/testrole", config);
+        Pair<String, String> credentials = getAppRoleCredentials(template, "auth/" + authMountPath + "/role/testrole");
+
+
+        final Pair<String, String> wrapped = VaultConnector.doRequestWrappedToken(new VaultFeatureSettings("", getVault().getUrl(), authMountPath, credentials.getFirst(), credentials.getSecond(), true));
+
+        then(wrapped.getFirst()).isNotNull();
+        then(wrapped.getSecond()).isNotNull();
+
+
+        final CubbyholeAuthenticationOptions options = CubbyholeAuthenticationOptions.builder()
+                .wrapped()
+                .initialToken(VaultToken.of(wrapped.getFirst()))
+                .build();
+        final RestTemplate simpleTemplate = UtilKt.createRestTemplate(new VaultFeatureSettings("", getVault().getUrl(), authMountPath, "", "", true));
+        final CubbyholeAuthentication authentication = new CubbyholeAuthentication(options, simpleTemplate);
+        final TaskScheduler scheduler = new ConcurrentTaskScheduler();
+
+        final MyLifecycleAwareSessionManager sessionManager = new MyLifecycleAwareSessionManager(authentication, scheduler, simpleTemplate, new LifecycleAwareSessionManager.FixedTimeoutRefreshTrigger(1L, TimeUnit.SECONDS), new NullBuildProgressLogger());
+
+        System.out.println("Once initialized = " + sessionManager.myRenewResults);
+        then(sessionManager.getSessionToken()).isNotNull();
+        System.out.println("After one check = " + sessionManager.myRenewResults);
+
+        for (int i = 0; i < 5; i++) {
+            ThreadUtil.sleep(1000L);
+            System.out.println("After one second sleep = " + sessionManager.myRenewResults);
+        }
+
+        then(sessionManager.getSessionToken()).isNotNull();
+        then(sessionManager.myRenewResults).isNotEmpty().doesNotContain(false);
     }
 
     private Pair<String, String> getAppRoleCredentials(VaultTemplate template, String path) {
@@ -118,13 +182,17 @@ public class VaultConnectorTest {
     }
 
     private static class MyLifecycleAwareSessionManager extends LifecycleAwareSessionManager {
-        private MyLifecycleAwareSessionManager(CubbyholeAuthentication authentication, RestTemplate simpleTemplate, TaskScheduler scheduler) {
-            super(authentication, scheduler, simpleTemplate);
+        List<Boolean> myRenewResults = new ArrayList<>();
+
+        public MyLifecycleAwareSessionManager(@NotNull ClientAuthentication clientAuthentication, @NotNull TaskScheduler taskScheduler, @NotNull RestOperations restOperations, @NotNull LifecycleAwareSessionManager.FixedTimeoutRefreshTrigger refreshTrigger, @NotNull BuildProgressLogger logger) {
+            super(clientAuthentication, taskScheduler, restOperations, refreshTrigger, logger);
         }
 
         @Override
         public boolean renewToken() {
-            return super.renewToken();
+            final boolean result = super.renewToken();
+            myRenewResults.add(result);
+            return result;
         }
     }
 }
