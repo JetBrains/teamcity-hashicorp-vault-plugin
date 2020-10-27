1ed77d9adadd0d0e3ba15dccb9c34090bdd81252
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2020-10-28 00:18:13 +0300

    Fix 'Fail On Error' property

--- common/src/main/kotlin/org/jetbrains/teamcity/vault/VaultFeatureSettings.kt
+++ common/src/main/kotlin/org/jetbrains/teamcity/vault/VaultFeatureSettings.kt
@@ -140,7 +140,7 @@ data class VaultFeatureSettings(val namespace: String, val url: String, val vaul
             map[VaultConstants.FeatureSettings.URL] ?: "",
             map[VaultConstants.FeatureSettings.VAULT_NAMESPACE]
                     ?: VaultConstants.FeatureSettings.DEFAULT_VAULT_NAMESPACE,
-            map[VaultConstants.FeatureSettings.FAIL_ON_ERROR]?.toBoolean() ?: true,
+            map[VaultConstants.FeatureSettings.FAIL_ON_ERROR]?.toBoolean() ?: false,
             Auth.fromProperties(map)
     )
 
@@ -168,6 +168,7 @@ data class VaultFeatureSettings(val namespace: String, val url: String, val vaul
                     VaultConstants.FeatureSettings.VAULT_NAMESPACE to VaultConstants.FeatureSettings.DEFAULT_VAULT_NAMESPACE,
                     VaultConstants.FeatureSettings.AGENT_SUPPORT_REQUIREMENT to VaultConstants.FeatureSettings.AGENT_SUPPORT_REQUIREMENT_VALUE,
                     VaultConstants.FeatureSettings.AUTH_METHOD to VaultConstants.FeatureSettings.DEFAULT_AUTH_METHOD,
+                    VaultConstants.FeatureSettings.FAIL_ON_ERROR to "true",
                     VaultConstants.FeatureSettings.ENDPOINT to VaultConstants.FeatureSettings.DEFAULT_ENDPOINT_PATH,
                     VaultConstants.FeatureSettings.URL to "http://localhost:8200"
             )
