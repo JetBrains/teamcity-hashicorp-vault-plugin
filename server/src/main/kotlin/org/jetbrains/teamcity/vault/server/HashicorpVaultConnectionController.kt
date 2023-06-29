3bbf11e09e75a9a4e268b7c4fbf36f84f16f9848
Andre Rocha <andre.rocha@jetbrains.com>
2023-06-29 10:20:22 +0000

    TW-82241 Get build id from the token
    Merge-request: TC-MR-5821
    Merged-by: André Rocha <andre.rocha@jetbrains.com>

--- server/src/main/kotlin/org/jetbrains/teamcity/vault/server/HashicorpVaultConnectionController.kt
+++ server/src/main/kotlin/org/jetbrains/teamcity/vault/server/HashicorpVaultConnectionController.kt
@@ -2,6 +2,7 @@ package org.jetbrains.teamcity.vault.server
 
 import jetbrains.buildServer.serverSide.BuildsManager
 import jetbrains.buildServer.serverSide.ProjectManager
+import jetbrains.buildServer.web.util.WebAuthUtil
 import org.jetbrains.teamcity.vault.VaultConstants
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping(VaultConstants.ControllerSettings.URL)
@@ -19,8 +21,9 @@ class HashicorpVaultConnectionController(
 ) {
 
     @RequestMapping(VaultConstants.ControllerSettings.WRAP_TOKEN_PATH, method = [RequestMethod.GET], produces = ["application/json"])
-    fun getToken(@RequestParam(name = "buildId") buildId: Long, @RequestParam(name = "namespace") namespace: String): Map<String, String> {
-        val build = buildsManager.findBuildInstanceById(buildId)
+    fun getToken(@RequestParam(name = "namespace") namespace: String, request: HttpServletRequest): Map<String, String> {
+        val buildId = WebAuthUtil.getAuthenticatedBuildId(request) ?: throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "No authenticated build has been found. You do not have access to this build")
+        val build = buildsManager.findRunningBuildById(buildId)
         if (build == null) {
             throw ResponseStatusException(HttpStatus.NOT_FOUND, "Build with id $buildId not found")
         }
