7e67ff17a22749fa0d11bc97d09c5d2c109ba680
evie.rocha <evie.rocha@jetbrains.com>
2024-06-21 08:37:38 +0000

    TW-88329 Move to TC retrier
    Merge-request: TC-MR-10740
    Merged-by: Évie Rocha <evie.rocha@jetbrains.com>

--- common/src/main/kotlin/org/jetbrains/teamcity/vault/retrier/ClientExceptionListener.kt
+++ common/src/main/kotlin/org/jetbrains/teamcity/vault/retrier/ClientExceptionListener.kt
@@ -1,12 +1,22 @@
 package org.jetbrains.teamcity.vault.retrier
 
 import jetbrains.buildServer.util.ExceptionUtil
+import jetbrains.buildServer.util.retry.AbortRetriesException
+import jetbrains.buildServer.util.retry.RetrierEventListener
+import java.lang.Exception
+import java.util.concurrent.Callable
 import kotlin.reflect.KClass
 
-abstract class ClientExceptionListener<T : Throwable>(val type: KClass<out T>) : Retrier.ExceptionListener {
+abstract class ClientExceptionListener<T : Throwable>(val type: KClass<out T>): RetrierEventListener {
     abstract fun isNonRecoverableKubernetesException(exception: T): Boolean
 
-    override fun isNonRecoverable(exception: Throwable): Boolean {
+    override fun <T : Any?> onFailure(callable: Callable<T>, retry: Int, e: Exception) {
+        if (isNonRecoverable(e)){
+            throw AbortRetriesException(e)
+        }
+    }
+
+    private fun isNonRecoverable(exception: Throwable): Boolean {
         val cause = ExceptionUtil.getCause(exception, type.java)
         return if (cause != null) {
             isNonRecoverableKubernetesException(cause)
