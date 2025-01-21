504b33493ab1baf581ad160d11b40e659cdd2d74
evie.rocha <evie.rocha@jetbrains.com>
2025-01-21 13:27:38 +0000

    TW-91564 Add ability to ignore connect timeout exceptions
    Merge-request: TC-MR-13822
    Merged-by: Évie Rocha <evie.rocha@jetbrains.com>

--- common/src/main/kotlin/org/jetbrains/teamcity/vault/retrier/VaultRetrier.kt
+++ common/src/main/kotlin/org/jetbrains/teamcity/vault/retrier/VaultRetrier.kt
@@ -1,13 +1,21 @@
 package org.jetbrains.teamcity.vault.retrier
 
 import jetbrains.buildServer.serverSide.TeamCityProperties
+import jetbrains.buildServer.util.retry.Retrier
+import org.apache.http.conn.ConnectTimeoutException
 
 object VaultRetrier {
     const val MAX_RETRIES = "teamcity.hashicorp.vault.maxRetries"
     const val RETRY_DELAY = "teamcity.hashicorp.vault.retryDelaySeconds"
 
-    fun getRetrier() = jetbrains.buildServer.util.retry.Retrier.withRetries(
-        TeamCityProperties.getInteger(VaultRetrier.MAX_RETRIES, 3),
-        jetbrains.buildServer.util.retry.Retrier.DelayStrategy.linearBackOff(TeamCityProperties.getInteger(VaultRetrier.RETRY_DELAY, 3) * 1000))
-        .registerListener(SpringHttpErrorCodeListener())
+    fun getRetrier(): Retrier {
+        val timeoutExceptionListener = object : ClientExceptionListener<ConnectTimeoutException>(ConnectTimeoutException::class) {
+            override fun isNonRecoverableKubernetesException(exception: ConnectTimeoutException): Boolean = false
+        }
+        return jetbrains.buildServer.util.retry.Retrier.withRetries(
+            TeamCityProperties.getInteger(VaultRetrier.MAX_RETRIES, 3),
+            jetbrains.buildServer.util.retry.Retrier.DelayStrategy.linearBackOff(TeamCityProperties.getInteger(VaultRetrier.RETRY_DELAY, 3) * 1000))
+            .registerListener(SpringHttpErrorCodeListener())
+            .registerListener(timeoutExceptionListener)
+    }
 }
